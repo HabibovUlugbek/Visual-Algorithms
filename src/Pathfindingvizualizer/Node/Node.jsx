0cd6614c81e6dcffffa8468fc2872b07fb4f85c9
HabibovUlugbek <habibovulugbek22@gmail.com>
2023-01-30 18:48:05 +0500

    vizual

---
+++ src/Pathfindingvizualizer/Node/Node.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+export default function Node({row, col, isFinish, isStart, isWall, onMouseDown, onMouseEnter, onMouseUp}) {
+
+    const extraClassName = isFinish
+      ? 'node-finish'
+      : isStart
+      ? 'node-start'
+      : isWall
+      ? 'node-wall'
+      : '';
+    return (
+      <div
+        id={`node-${row}-${col}`}
+        className={`node ${extraClassName}`}
+        onMouseDown={() => onMouseDown(row, col)}
+        onMouseEnter={() => onMouseEnter(row, col)}
+        onMouseUp={() => onMouseUp()}></div>
+    );
+}
